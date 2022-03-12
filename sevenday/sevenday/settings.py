49f222b84820cc6d0ff9c22c56a8bcddfd9a3cbc
Jiawei_Wang <1508417398@qq.com>
2022-03-12 23:37:12 +0800

    update

--- sevenday/sevenday/settings.py
+++ sevenday/sevenday/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+bzt@86g-9u&c5d0cclu_@mfohjh$$*nu#pg@pu3!=6^%$eju'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
